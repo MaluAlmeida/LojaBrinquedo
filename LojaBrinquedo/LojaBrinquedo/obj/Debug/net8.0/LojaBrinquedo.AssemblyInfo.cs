779cba50a6a78e965c6972714d0530922c4453ad
MaluAlmeida <malugoncalves2707@gmail.com>
2025-09-16 08:02:21 -0300

    Subindo projeto

--- LojaBrinquedo/LojaBrinquedo/obj/Debug/net8.0/LojaBrinquedo.AssemblyInfo.cs
+++ LojaBrinquedo/LojaBrinquedo/obj/Debug/net8.0/LojaBrinquedo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LojaBrinquedo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e666f09702fe341d6cc2a0e383a8f72bb233338")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+263e37b96db45215cc799b3bbe79dad939c0bab7")]
 [assembly: System.Reflection.AssemblyProductAttribute("LojaBrinquedo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LojaBrinquedo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
